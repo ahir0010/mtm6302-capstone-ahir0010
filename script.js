b0283a2beb00d6bab094aa5bd2fd7e5f575d755e
Hemil <ahir0010@algonquinlive.com>
2024-04-14 21:08:22 -0400

    Completes the assignment

---
+++ script.js
@@ -0,0 +1,31 @@
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=20';
+let nextUrl = '';
+
+document.getElementById('loadMore').addEventListener('click', loadPokemons);
+
+function loadPokemons() {
+    fetch(nextUrl || apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            nextUrl = data.next;
+            data.results.forEach(pokemon => {
+                fetch(pokemon.url)
+                    .then(response => response.json())
+                    .then(pokeData => {
+                        const pokemonContainer = document.getElementById('pokemonContainer');
+                        const pokemonCard = document.createElement('div');
+                        // Added responsive classes: 'col-sm-6' for small devices and 'col-md-2' for medium to larger devices
+                        pokemonCard.classList.add('col-sm-6', 'col-md-2', 'pokemon-card');
+                        pokemonCard.innerHTML = `
+                            <img src="${pokeData.sprites.front_default}" alt="${pokeData.name}" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">${pokeData.name}</h5>
+                            </div>
+                        `;
+                        pokemonContainer.appendChild(pokemonCard);
+                    });
+            });
+        });
+}
+loadPokemons();
+
